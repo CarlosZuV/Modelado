2aca67e566fb5da60959bc92cda82fa4903eaa75
DonitaViolenta <donitaprogramadora@gmail.com>
2026-09-19 23:08:32 -0600

    cambio en el main para ver si el modulo 1 funciona

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
+#include"../include/lector_ruta.h"
+#include<string>
+#include<filesystem>
+#include<stdexcept>
+#include<iostream>
 
 int main() {
-    return 0;
+  std::string alfa;
+
+  std::cin >> alfa;
+
+  lector_ruta ruta(alfa);
+
+  if(ruta.esUsable()) {
+    std::cout << "Si funco esta wea" << '\n' << "La ruta es" << alfa << '\n';
+  } else {
+    std::cout << "NO funco esta wea" << '\n';
+  }
+  return 0;
 }
